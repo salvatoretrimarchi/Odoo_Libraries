46aef94181c95e1fc916c9e68606c8a3a343d1b4
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-01-07 15:44:01 -0500

    Modulo de Detracciones Completo

--- detracciones/models/models.py
+++ detracciones/models/models.py
@@ -6,6 +6,8 @@ from odoo import models, fields, api
 class detracciones(models.Model):
     _name = 'sunat.detracciones'
     _description = "Codigos de Detracciones"
+    _order = 'codigo'
+    _rec_name = 'codigo'
 
     codigo = fields.Char(string="Código")
     desc = fields.Text(string="Descripción")
