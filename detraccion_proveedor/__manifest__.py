344df5b7c34d84db386b6fb08256ea1283e99e23
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-01-08 11:50:35 -0500

    Update __manifest__.py

--- detraccion_proveedor/__manifest__.py
+++ detraccion_proveedor/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "Proveedor Detracciones",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Integracion de las Detracciones con los Proveedores y Facturas""",
 
     'description': """
         Long description of module's purpose
