071e2eeacde9661172824d8e8a4457faa5272345
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-01-09 11:02:08 -0500

    Un solo campo para el codigo y descripcion de la detraccion

--- detracciones/models/models.py
+++ detracciones/models/models.py
@@ -5,14 +5,12 @@ from odoo import models, fields, api
 class detracciones(models.Model):
     _name = 'sunat.detracciones'
     _description = "Codigos de Detracciones"
-    _order = 'codigo'
 
-    name = fields.Char(string="Porcentaje",compute="_obtener_detraccion")
-    codigo = fields.Char(string="Código")
-    desc = fields.Text(string="Descripción")
+    name = fields.Text(string="Descripción")
     detrac = fields.Float(string="Detraccion")
+    detracmack = fields.Char(string="Porcentaje",compute="_obtener_detraccion")
 
     def _obtener_detraccion(self):
         for rec in self:
             detrac = str(rec.detrac)
-            rec.name = "{} %".format(detrac)
+            rec.detracmack = "{} %".format(detrac)
