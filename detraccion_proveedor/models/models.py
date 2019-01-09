59357e09f462901190fb175d9c6c57343c8e2940
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-01-09 01:19:20 -0500

    Calculo de Detraccion por Factura

--- detraccion_proveedor/models/models.py
+++ detraccion_proveedor/models/models.py
@@ -16,11 +16,17 @@ class account_invoice(models.Model):
     _inherit = "account.invoice"
 
     detrac_id = fields.Many2one('sunat.detracciones', 'Detraccion')
+    detraccion = fields.Monetary(compute='_calcular_detrac',store=True)
 
     @api.onchange('partner_id')
     def _onchange_proveedor(self):
 #        if len(self.detrac_id) <= 0 :
             self.detrac_id = self.partner_id.detrac_id
+
+    
+    @api.onchange('amount_total')
+    def _calcular_detrac(self):
+        self.detraccion = self.amount_total * (self.detrac_id.detrac / 100)
 
 
 class detracciones(models.Model):
--- detracciones/models/models.py
+++ detracciones/models/models.py
@@ -6,8 +6,13 @@ class detracciones(models.Model):
     _name = 'sunat.detracciones'
     _description = "Codigos de Detracciones"
     _order = 'codigo'
-    _rec_name = 'desc'
 
+    name = fields.Char(string="Porcentaje",compute="_obtener_detraccion")
     codigo = fields.Char(string="Código")
     desc = fields.Text(string="Descripción")
     detrac = fields.Float(string="Detraccion")
+
+    def _obtener_detraccion(self):
+        for rec in self:
+            detrac = str(rec.detrac)
+            rec.name = "{} %".format(detrac)
